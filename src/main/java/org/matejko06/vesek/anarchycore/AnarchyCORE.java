96e4577869020895fe2ee35be42ba2d50d1b8441
Matejko06 <67696646+Matejko06@users.noreply.github.com>
2020-08-22 17:24:46 +0200

    AnarchyCORE 1.2.5

--- src/main/java/org/matejko06/vesek/anarchycore/AnarchyCORE.java
+++ src/main/java/org/matejko06/vesek/anarchycore/AnarchyCORE.java
@@ -1,5 +1,6 @@
 package org.matejko06.vesek.anarchycore; //hehe
 
+import org.apache.commons.io.FileUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -10,8 +11,18 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import java.io.File;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public final class AnarchyCORE extends JavaPlugin implements Listener {
@@ -85,6 +96,49 @@ public final class AnarchyCORE extends JavaPlugin implements Listener {
                     sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("queue-message")) + ChatColor.translateAlternateColorCodes('&', getConfig().getString("regular-message")));
                 } else {
                     sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("invalid-command")));
+                }
+            }
+        }  else if (command.getName().equalsIgnoreCase("info") && sender instanceof Player) {
+            if(sender instanceof Player){
+                Player p = (Player) sender;
+                String s = this.getConfig().getString("info-message");
+                try {
+                    File world = p.getWorld().getWorldFolder();
+                    File playerData = new File(world.getAbsolutePath() + "\\playerdata");
+                    File data = new File(this.getServer().getWorldContainer().getAbsolutePath() + "/eula.txt");
+                    BasicFileAttributes attr = Files.readAttributes(data.toPath(), BasicFileAttributes.class);
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                    Date firstDate = sdf.parse(attr.creationTime().toString());
+                    Date secondDate = sdf.parse(LocalDate.now().toString());
+                    int diffInDays = (int)((secondDate.getTime() - firstDate.getTime()) / 86400000L);
+                    StringBuilder time = new StringBuilder();
+                    String[] sa = playerData.list();
+                    int people = 0;
+                    for(String sas : sa){
+                        if(sas.split("\\.")[1].equals("dat")){
+                            people++;
+                        }
+                    }
+                    if (diffInDays / 365 < 1) {
+                        time.append(diffInDays / 30 + " months");
+                    }
+                    else if (diffInDays / 365 == 1) {
+                        diffInDays -= 365;
+                        time.append("1 year and ");
+                        time.append(diffInDays / 30 + " months");
+                    }
+                    else if (diffInDays / 365 > 1) {
+                        time.append(diffInDays / 365 + " years and ");
+                        diffInDays %= 365;
+                        time.append(diffInDays / 30 + " months");
+                    }
+                    s = s.replace("<players>", String.valueOf(people));
+                    s = s.replace("<time>", time.toString());
+                    s = s.replace("<size>", FileUtils.byteCountToDisplaySize(world.length()));
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', s));
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         }
