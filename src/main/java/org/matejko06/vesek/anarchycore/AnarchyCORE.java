f5ca6e8406dcfc806d6a044017b84f17a0aec4a9
Vesek <venda.straka@gmail.com>
2020-08-23 19:41:21 +0200

    Moved events to separate class.

--- src/main/java/org/matejko06/vesek/anarchycore/AnarchyCORE.java
+++ src/main/java/org/matejko06/vesek/anarchycore/AnarchyCORE.java
@@ -1,28 +1,16 @@
 package org.matejko06.vesek.anarchycore;
 
-import org.apache.commons.io.FileUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.matejko06.vesek.anarchycore.commands.*;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-public final class AnarchyCORE extends JavaPlugin implements Listener {
+public final class AnarchyCORE extends JavaPlugin{
 
     public static boolean command_preprocessing = false;
 
@@ -33,6 +21,7 @@ public final class AnarchyCORE extends JavaPlugin implements Listener {
     InfoCommand ic = new InfoCommand(this);
     HelpCommand hc = new HelpCommand(this);
     ReloadCommand rc = new ReloadCommand(this);
+    Events events = new Events(this);
 
     public void log(String text) {
         Bukkit.getConsoleSender().sendMessage(text);
@@ -42,7 +31,7 @@ public final class AnarchyCORE extends JavaPlugin implements Listener {
     public void onEnable() {
         getConfig().options().copyDefaults(true);
         saveConfig();
-        getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(events, this);
         command_preprocessing = getConfig().getBoolean("command-preprocessing");
         //getCommand("acversion").setExecutor(vc);
         getCommand("tps").setExecutor(tc);
@@ -74,24 +63,5 @@ public final class AnarchyCORE extends JavaPlugin implements Listener {
             }
         }
         return null;
-    }
-
-
-
-    @EventHandler
-    public void onCommandPreProcess(PlayerCommandPreprocessEvent event){
-        if(event.getMessage().toLowerCase().startsWith("/tps") && command_preprocessing){
-            event.setCancelled(true);
-            event.getPlayer().chat("/anarchycore:tps");
-        }
-    }
-
-    @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event){
-        String dm = event.getDeathMessage();
-        event.setDeathMessage(null);
-        for(Player p : getServer().getOnlinePlayers()){
-            p.sendMessage(dm);
-        }
     }
 }
---
+++ src/main/java/org/matejko06/vesek/anarchycore/Events.java
@@ -0,0 +1,33 @@
+package org.matejko06.vesek.anarchycore;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.Plugin;
+
+public class Events implements Listener {
+
+    org.bukkit.plugin.Plugin plugin;
+    public Events (Plugin plugin){
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onCommandPreProcess(PlayerCommandPreprocessEvent event){
+        if(event.getMessage().toLowerCase().startsWith("/tps") && AnarchyCORE.command_preprocessing){
+            event.setCancelled(true);
+            event.getPlayer().chat("/anarchycore:tps");
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event){
+        String dm = event.getDeathMessage();
+        event.setDeathMessage(null);
+        for(Player p : plugin.getServer().getOnlinePlayers()){
+            p.sendMessage(dm);
+        }
+    }
+}
