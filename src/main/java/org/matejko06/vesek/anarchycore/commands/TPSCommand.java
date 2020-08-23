6c3bb1d21a5b02cc6f89cb5a055a6edb57c32994
Matejko06 <67696646+Matejko06@users.noreply.github.com>
2020-08-23 13:42:55 +0200

    Update TPSCommand.java

--- src/main/java/org/matejko06/vesek/anarchycore/commands/TPSCommand.java
+++ src/main/java/org/matejko06/vesek/anarchycore/commands/TPSCommand.java
@@ -2,3 +2,16 @@ package org.matejko06.vesek.anarchycore.commands;
 
 public class TPSCommand {
 }
+//        } else if (command.getName().equalsIgnoreCase("tps")) {
+//                if (sender.hasPermission("ValueSkinsCORE.tps") || sender.isOp()) {
+//                StringBuilder sb = new StringBuilder();
+//                double[] TPS = getServer().getTPS();
+//                if (TPS[0] >= getConfig().getDouble("tps-green")) {
+//                sb.append(ChatColor.translateAlternateColorCodes('&', getConfig().getString("tps-message"))).append(ChatColor.translateAlternateColorCodes('&', getConfig().getString("tps-green-color"))).append(String.format("%.2f", TPS[0]));
+//                } else if (TPS[0] >= getConfig().getDouble("tps-yellow")) {
+//                sb.append(ChatColor.translateAlternateColorCodes('&', getConfig().getString("tps-message"))).append(ChatColor.translateAlternateColorCodes('&', getConfig().getString("tps-yellow-color"))).append(String.format("%.2f", TPS[0]));
+//                } else if (TPS[0] >= 0.00) {
+//                sb.append(ChatColor.translateAlternateColorCodes('&', getConfig().getString("tps-message"))).append(ChatColor.translateAlternateColorCodes('&', getConfig().getString("tps-red-color"))).append(String.format("%.2f", TPS[0]));
+//                }
+//                sender.sendMessage(sb.toString());
+//                }
