6c82f41e82ce4bff7f1d7ee8a0ac3c49ba8e3db6
Matejko06 <67696646+Matejko06@users.noreply.github.com>
2020-08-23 10:08:16 +0200

    AnarchyCORE 1.2.9

--- src/main/java/org/matejko06/vesek/anarchycore/AnarchyCORE.java
+++ src/main/java/org/matejko06/vesek/anarchycore/AnarchyCORE.java
@@ -11,8 +11,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.File;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.text.SimpleDateFormat;
@@ -89,65 +93,63 @@ public final class AnarchyCORE extends JavaPlugin implements Listener {
                 } else if (sender.hasPermission("AnarchyCOREQueue.priority")) {
                     sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("queue-message")) + ChatColor.translateAlternateColorCodes('&', getConfig().getString("priority-message")));
                 } else if (sender.hasPermission("AnarchyCOREQueue.regular")) {
-                    if (sender.hasPermission("AnarchyCORE.queue.admin")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("queue-message")) + ChatColor.translateAlternateColorCodes('&', getConfig().getString("admin-message")));
-                    } else if (sender.hasPermission("AnarchyCORE.queue.priority")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("queue-message")) + ChatColor.translateAlternateColorCodes('&', getConfig().getString("priority-message")));
-                    } else if (sender.hasPermission("AnarchyCORE.queue.regular")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("queue-message")) + ChatColor.translateAlternateColorCodes('&', getConfig().getString("regular-message")));
-                    } else {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("invalid-command")));
-                    }
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("queue-message")) + ChatColor.translateAlternateColorCodes('&', getConfig().getString("regular-message")));
+                } else {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("invalid-command")));
                 }
-            } else if (command.getName().equalsIgnoreCase("info")) {
-                if (sender instanceof Player) {
-                    Player p = (Player) sender;
-                    String s = this.getConfig().getString("info-message");
-                    try {
-                        File world = p.getWorld().getWorldFolder();
-                        File playerData = new File(world.getAbsolutePath() + "\\playerdata");
-                        File data = new File(this.getServer().getWorldContainer().getAbsolutePath() + "/eula.txt");
-                        BasicFileAttributes attr = Files.readAttributes(data.toPath(), BasicFileAttributes.class);
-                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                        Date firstDate = sdf.parse(attr.creationTime().toString());
-                        Date secondDate = sdf.parse(LocalDate.now().toString());
-                        int diffInDays = (int) ((secondDate.getTime() - firstDate.getTime()) / 86400000L);
-                        StringBuilder time = new StringBuilder();
-                        String[] sa = playerData.list();
-                        int people = 0;
-                        if (sa != null) {
-                            for (String sas : sa) {
-                                if (sas.split("\\.")[1].equals("dat")) {
-                                    people++;
-                                }
-                            }
+            }
+        }  else if (command.getName().equalsIgnoreCase("info") && sender instanceof Player) {
+            if(sender instanceof Player){
+                Player p = (Player) sender;
+                String s = this.getConfig().getString("info-message");
+                try {
+                    File world = p.getWorld().getWorldFolder();
+                    File playerData = new File(world.getAbsolutePath() + "\\playerdata");
+                    File data = new File(this.getServer().getWorldContainer().getAbsolutePath() + "/eula.txt");
+                    BasicFileAttributes attr = Files.readAttributes(data.toPath(), BasicFileAttributes.class);
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                    Date firstDate = sdf.parse(attr.creationTime().toString());
+                    Date secondDate = sdf.parse(LocalDate.now().toString());
+                    int diffInDays = (int)((secondDate.getTime() - firstDate.getTime()) / 86400000L);
+                    StringBuilder time = new StringBuilder();
+                    String[] sa = playerData.list();
+                    int people = 0;
+                    for(String sas : sa){
+                        if(sas.split("\\.")[1].equals("dat")){
+                            people++;
                         }
-                        if (diffInDays / 365 < 1) {
-                            if (diffInDays / 30 >= 1) {
-                                time.append(diffInDays / 30).append(" months and ");
-                                diffInDays %= 30;
-                            }
-                            time.append(diffInDays).append(" days");
-                        } else if (diffInDays / 365 == 1) {
-                            diffInDays -= 365;
-                            time.append("1 year and ");
-                            time.append(diffInDays / 30).append(" months");
-                        } else if (diffInDays / 365 > 1) {
-                            time.append(diffInDays / 365).append(" years and ");
-                            diffInDays %= 365;
-                            time.append(diffInDays / 30).append(" months");
-                        }
-                        s = s.replace("<players>", String.valueOf(people));
-                        s = s.replace("<time>", time.toString());
-                        s = s.replace("<size>", FileUtils.byteCountToDisplaySize(world.length()));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', s));
-                    } catch (Exception e) {
-                        e.printStackTrace();
                     }
+                    if (diffInDays / 365 < 1) {
+                        if (diffInDays / 30 < 1) {
+                            time.append(diffInDays + " days");
+                        }
+                        else{
+                            time.append(diffInDays / 30 + " months and ");
+                            diffInDays %= 30;
+                            time.append(diffInDays + " days");
+                        }
+                    }
+                    else if (diffInDays / 365 == 1) {
+                        diffInDays -= 365;
+                        time.append("1 year and ");
+                        time.append(diffInDays / 30 + " months");
+                    }
+                    else if (diffInDays / 365 > 1) {
+                        time.append(diffInDays / 365 + " years and ");
+                        diffInDays %= 365;
+                        time.append(diffInDays / 30 + " months");
+                    }
+                    s = s.replace("<players>", String.valueOf(people));
+                    s = s.replace("<time>", time.toString());
+                    s = s.replace("<size>", FileUtils.byteCountToDisplaySize(world.length()));
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', s));
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         }
-            return false;
+        return false;
     }
 
     @Override
