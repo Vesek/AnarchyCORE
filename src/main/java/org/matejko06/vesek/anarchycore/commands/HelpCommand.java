e11d620d51ef2340103dd461e32aa6635fb7d50b
Matejko06 <67696646+Matejko06@users.noreply.github.com>
2020-08-24 08:01:51 +0200

    Help added

--- src/main/java/org/matejko06/vesek/anarchycore/commands/HelpCommand.java
+++ src/main/java/org/matejko06/vesek/anarchycore/commands/HelpCommand.java
@@ -4,8 +4,8 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-import org.matejko06.vesek.anarchycore.AnarchyCORE;
 
 public class HelpCommand implements CommandExecutor {
     Plugin plugin;
@@ -15,13 +15,15 @@ public class HelpCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args[0].equalsIgnoreCase("help")) {
-            if (sender.hasPermission("AnarchyCORE.help") || sender.isOp()) {
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&3<----------------------->\n&6&o/help &3- Shows this.\n&6&o/kill &3- Kills the player.\n&6&o/tps &3- Shows server TPS.\n&6&o/info &3- Shows some info about the server.\n&6&o/priority &3or &6&o/queue &3- Shows players queue priority.\n&3<----------------------->"));
-            }
-        } else if (sender.hasPermission("AnarchyCORE.tps") || sender.isOp()) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&3<----------------------->\n&6&o/help &3- Shows this.\n&6&o/kill &3- Kills the player.\n&6&o/tps &3- Shows server TPS.\n&6&o/info &3- Shows some info about the server.\n&6&o/priority &3or &6&o/queue &3- Shows players queue priority.\n&3<----------------------->"));
+        if (command.getName().equalsIgnoreCase("help")) {
+                if (args.length == 0) {
+                    if (sender.hasPermission("AnarchyCORE.help") || sender.isOp()) {
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("help-message")));
                     }
+                }
+            } else {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("help-message")));
+            }
         return false;
     }
 }
