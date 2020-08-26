bfba6d5ee54814866837fa91f2142cb3150c2f58
Matejko06 <67696646+Matejko06@users.noreply.github.com>
2020-08-26 22:21:11 +0200

    UPDATE

--- src/main/java/org/matejko06/vesek/anarchycore/Events.java
+++ src/main/java/org/matejko06/vesek/anarchycore/Events.java
@@ -1,6 +1,7 @@
 package org.matejko06.vesek.anarchycore;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -16,6 +17,11 @@ public class Events implements Listener {
         this.plugin = plugin;
     }
 
+        AnarchyCORE ac;
+        public Events(AnarchyCORE ac) {
+            this.ac = ac;
+        }
+
     @EventHandler
     public void onCommandPreProcess(PlayerCommandPreprocessEvent event) {
         if (event.getMessage().toLowerCase().startsWith("/tps") && AnarchyCORE.command_preprocessing) {
@@ -24,11 +30,11 @@ public class Events implements Listener {
         }
     }
 
-    /*@EventHandler
+    @EventHandler
     public void onChat(AsyncPlayerChatEvent e){
         String s = e.getMessage();
         if(s.charAt(0) == '>'){
             e.setMessage(ChatColor.GREEN + e.getMessage().substring(1,s.length()));
         }
-    }*/
+    }
 }
